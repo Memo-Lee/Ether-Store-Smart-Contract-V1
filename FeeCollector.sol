9da361e6504f1c6f57edcefae677e1f93818d401
Mehmet Sonmez <101952360+Memo-Lee@users.noreply.github.com>
2022-09-28 18:38:00 +0300

    Update FeeCollector.sol

--- FeeCollector.sol
+++ FeeCollector.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
-// error veriyor
 contract FreeCollector { // 0x87... gibi contract address gönderilme adresi
         address public owner;
         uint256 public balance;
@@ -12,7 +11,9 @@ contract FreeCollector { // 0x87... gibi contract address gönderilme adresi
         }
 
         function withdraw(uint amount, address payable destAddr) public {
+            // Para çekme işlemini sadece 'owner' (sahibi) yapabilir.
             require (msg.sender == owner , "Only owner can withdraw");
+            // tutar <= bakiye'den küçük ise "Yetersiz bakiye" require'ı döndürülür
             require (amount <= balance , "Insufficient funds");
             
             destAddr.transfer(amount);
